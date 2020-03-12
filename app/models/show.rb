2a5635ad8a130ee8af9da83353f27c0f17906e6f
S. Makai Takori <makait87@gmail.com>
2020-03-12 14:43:44 +0000

    Automatically backed up by Learn

--- app/models/show.rb
+++ app/models/show.rb
@@ -14,8 +14,10 @@ class Show < ActiveRecord::Base
   
   def Show::least_popular_show
     Show.order(rating: :desc).last
-  
   end
   
+  def Show::ratings_sum
+    Show.sum("rating")
+  end
   
 end
